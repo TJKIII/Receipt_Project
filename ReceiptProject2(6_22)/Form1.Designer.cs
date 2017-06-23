e0064bb641c014b315efbe44beed93549cfac4a3
TK <TK@DESKTOP-QUAEPTE>
2017-06-23 10:57:18 -0400

    First 2 Listboxes working

---
+++ ReceiptProject2(6_22)/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ReceiptProject2_6_22_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.receipts_DBDataSet = new ReceiptProject2_6_22_.Receipts_DBDataSet();
+            this.groceryInventoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.grocery_InventoryTableAdapter = new ReceiptProject2_6_22_.Receipts_DBDataSetTableAdapters.Grocery_InventoryTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receipts_DBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groceryInventoryBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 404);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(256, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(344, 404);
+            this.listBox2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(607, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1307, 403);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // receipts_DBDataSet
+            // 
+            this.receipts_DBDataSet.DataSetName = "Receipts_DBDataSet";
+            this.receipts_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // groceryInventoryBindingSource
+            // 
+            this.groceryInventoryBindingSource.DataMember = "Grocery_Inventory";
+            this.groceryInventoryBindingSource.DataSource = this.receipts_DBDataSet;
+            // 
+            // grocery_InventoryTableAdapter
+            // 
+            this.grocery_InventoryTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label1.Location = new System.Drawing.Point(119, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 91);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1926, 855);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receipts_DBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groceryInventoryBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Receipts_DBDataSet receipts_DBDataSet;
+        private System.Windows.Forms.BindingSource groceryInventoryBindingSource;
+        private Receipts_DBDataSetTableAdapters.Grocery_InventoryTableAdapter grocery_InventoryTableAdapter;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ReceiptProject2(6_22)/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ReceiptProject2_6_22_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetData1();
+        }
+
+        //Allowing a public connection accessible to all columns
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-QUAEPTE;Initial Catalog=Receipts_DB;Integrated Security=True");
+        SqlCommand command = new SqlCommand();
+        SqlDataReader datapop1,datapop2, datapop3, eventdata1, eventdata2;
+
+        string GroceryCoString, StoreAddress, StoreID;
+
+        
+
+       private void SetData1() //This populates the first table
+        {
+           command.Connection = con;
+           con.Open();
+           command.CommandText = "Select Company_Name from Companies";
+            datapop1 = command.ExecuteReader();
+            if (datapop1.HasRows)
+            {
+                while (datapop1.Read())
+                {
+                    listBox1.Items.Add(datapop1[0]);
+                }
+            }
+            con.Close();
+            }
+
+        private void SetData2(string GroceryID) //This populates the first table
+        {
+            //listBox2.Items.Clear();
+            command.Connection = con;
+            con.Open();
+            command.CommandText = "Select StreetAddress FROM Stores WHERE GroceryCompany_ID = '" + GroceryID + "'";
+           // command.CommandText = "Select StreetAddress FROM Stores";
+            datapop2 = command.ExecuteReader();
+            if (datapop2.HasRows)
+            {
+                while (datapop2.Read())
+                {
+                    listBox2.Items.Add(datapop2[0]);
+                }
+            }
+            con.Close(); 
+
+            //string StoreAdress = listBox2.SelectedIndex.ToString();
+          // MessageBox.Show(StoreAddress);
+
+            
+        }
+
+        private void SetData3(string StoreID)
+        {
+            // MessageBox.Show(StoreID);
+         
+            
+        }
+
+
+        
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string GroceryName = listBox1.SelectedItem.ToString();
+            con.Open();
+            command.Connection = con; 
+            command.CommandText = "Select GroceryCompany_ID from Companies WHERE Company_Name = '"+GroceryName+"'";
+            eventdata1 = command.ExecuteReader();
+            eventdata1.Read();
+            GroceryCoString = eventdata1[0].ToString();
+            con.Close(); //closing the connection so a new one can be opened in SetData(2)
+            listBox2.Items.Clear();
+            SetData2(GroceryCoString); 
+
+        }
+
+        private void listBox2_SelectedValueChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("LOL IS THIS THING ON?");
+                /*
+            string StoreAddress = listBox2.SelectedItem.ToString();
+            MessageBox.Show(StoreAddress);
+           
+              con.Open();
+                command.Connection = con;
+                command.CommandText = "Select Store_ID from Stores WHERE StreetAddress = '" + StoreAddress + "'";
+                eventdata2 = command.ExecuteReader();
+                eventdata2.Read();
+                StoreID = eventdata2[0].ToString();
+                con.Close(); //closing the connection so a new one can be opened in SetData(2)
+
+            MessageBox.Show(StoreID);
+
+            SetData3(StoreID);
+*/
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //when the form loads it opens the connection
+            command.Connection = con; 
+
+            // TODO: This line of code loads data into the 'receipts_DBDataSet.Grocery_Inventory' table. You can move, or remove it, as needed.
+            //this.grocery_InventoryTableAdapter.Fill(this.receipts_DBDataSet.Grocery_Inventory);
+
+        }
+        
+    }
+}
